ac8bf8d2ce97348e011459cfd8f7721c8229da01
onomatopoetica <hi.jen@icloud.com>
2022-05-26 14:19:21 -0700

    Added styles + seeds for app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
-class ApplicationController < ActionController::Base
+class QuotesController < ApplicationController
+  # ...
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_path, notice: "Quote was successfully created."
+    else
+      # Add `status: :unprocessable_entity` here
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # ...
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: "Quote was successfully updated."
+    else
+      # Add `status: :unprocessable_entity` here
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # ...
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,4 +1,4 @@
-# require 'test_helper'
+require 'test_helper'
 
 class Quote < ApplicationRecord
   validates :name, presence: true
