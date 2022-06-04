95554b29c2a73da3190ab05d273c8305ebbb5245
onomatopoetica <hi.jen@icloud.com>
2022-06-03 17:10:51 -0700

    Updated quotes_controller + application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,6 @@
-class QuotesController < ApplicationController
-  # ...
 
-  def create
-    @quote = Quote.new(quote_params)
-
-    if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
-    else
-      # Add `status: :unprocessable_entity` here
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # ...
-
-  def update
-    if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
-    else
-      # Add `status: :unprocessable_entity` here
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # ...
+class ApplicationController < ActionController::Base
+  # Add this line to see the progress bar long enough
+  # and remove it when it has the expected styles
+  before_action -> { sleep 3 }
 end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,7 +18,8 @@ class QuotesController < ApplicationController
     if @quote.save
       redirect_to quotes_path, notice: "Quote was successfully created."
     else
-      render :new
+      # Add `status: :unprocessable_entity` here
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -29,7 +30,8 @@ class QuotesController < ApplicationController
     if @quote.update(quote_params)
       redirect_to quotes_path, notice: "Quote was successfully updated."
     else
-      render :edit
+      # Add `status: :unprocessable_entity` here
+      render :edit, status: :unprocessable_entity
     end
   end
 
